d631413da3b448bd4dc0b1060aa13aad72bb34d9
Zbyszko Papierski <zbyszko.papierski@allegro.pl>
2015-06-21 07:31:22 +0200

    commenting functionality added 2

--- jug-factor-service/src/main/scala/pl/jug/torun/jugfactor/service/core/AnnotationEventOutput.scala
+++ jug-factor-service/src/main/scala/pl/jug/torun/jugfactor/service/core/AnnotationEventOutput.scala
@@ -1,3 +1,3 @@
 package pl.jug.torun.jugfactor.service.core
 
-case class AnnotationEventOutput(/*id: org.bson.types.ObjectId, */offset: Long, eventType: String)
+case class AnnotationEventOutput(/*id: org.bson.types.ObjectId, */offset: Long, eventType: String, comment:Option[String] = None)
