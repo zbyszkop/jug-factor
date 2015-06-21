30eb85c1b03616fff42dd2fd6525720c15c84e1c
Krzysztof Pobożan <krzpob@gmail.com>
2015-06-21 08:33:49 +0200

    admin

--- jug-factor-player/app/admin/admin.js
+++ jug-factor-player/app/admin/admin.js
@@ -9,23 +9,52 @@ angular.module('myApp.admin', ['ngRoute', 'ngResource'])
     .factory('Presentation',['$resource',function($resource){
         return $resource('http://localhost:8080/presentation');
     }])
-    .factory('Events',['$resource'],function($resource){
-        return $resource('http://localhost:8080/annotationEvent');
-    })
-    .controller('AdminCtrl',['$scope','Presentation',function($scope,Presentation,Events){
+    .controller('AdminCtrl',['$scope','$resource','$interval','Presentation',function($scope,$resource,$interval,Presentation){
         $scope.presentation=new Presentation();
-        $events=[];
-        new Events().query(
-            {eventType:'clapper'}
-        ).$promise.then(function(data){
-            console.log(data);
+        $scope.events = [];
+        $scope.clapper={};
+        //new Events().query(
+        //    {eventType:'clapper'}
+        //).$promise.then(function(data){
+        //    console.log(data);
+        //});
+        var evnentsResource = new $resource('http://localhost:8080/annotationEvent');
+        evnentsResource.query({eventType:'clapper'}).$promise.then(function(result){
+            $scope.events=result;
         });
 
+        var checkDuration={};
+
+        $scope.$watch('presentation.url',function(){
+            if($scope.presentation.url!=undefined){
+                $scope.pop=Popcorn.smart( "#video", $scope.presentation.url);
+                $scope.pop.controls( true );
+                $scope.pop.play();
+                checkDuration = $interval(function() {
+                        $scope.presentation.duration = $scope.pop.duration();
+                        if($scope.presentation.duration!=NaN){
+                            $interval.cancel(checkDuration);
+                        }
+                    },1000
+                )
+
+            } else {
+
+                delete $scope.pop;
+            }
+        },true)
 
 
         $scope.save= function(){
-            $scope.presentation.duration = Popcorn.smart( "#video", $scope.presentation.url).duration();
-            $scope.presentation.startTime=event.time;
+            if($scope.presentation.duration!=NaN){
+                $interval.cancel(checkDuration);
+                $scope.presentation.duration = $scope.pop.duration();
+
+                $scope.presentation.startTime=$scope.clapper.timestamp;
+                console.log($scope.presentation);
+                $scope.presentation.$save();
+                window.location.reload();
+            }
         }
 
     }]);
--- jug-factor-player/app/app.js
+++ jug-factor-player/app/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.player'
+  'myApp.player',
+  'myApp.admin'
 ])
 .value('serverUrl', 'http://localhost:8080')
 .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
